718ac402d278d60c5a8d3d34edebde59b1f677e6
Kirin <kirinlyraota.work@gmail.com>
2026-02-18 16:10:40 +0000

    Second Variables Alteration: characters

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -10,9 +10,19 @@ namespace VariousTypes
             int numero_1 = 6;
             string nome = "Nuno";
             bool booleano = true;
+            char character_1 = '\uFDFD';
+            char character_2 = '\u2661';
+            char character_3 = '\u1D4F';
+            char character_4 = '\u2070';
+            char character_5 = '\u0007';
             Console.WriteLine(numero_1);
             Console.WriteLine(nome);
             Console.WriteLine(booleano);
+            Console.WriteLine(character_1);
+            Console.WriteLine(character_2);
+            Console.WriteLine(character_3);
+            Console.WriteLine(character_4);
+            Console.WriteLine(character_5);
         }
     }
 }
