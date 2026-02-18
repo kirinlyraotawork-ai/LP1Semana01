790aeacc2fbac5e985e8320a4db667198d6b0d4b
Kirin <kirinlyraota.work@gmail.com>
2026-02-18 16:17:35 +0000

    Error Fix: variable bool

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -9,20 +9,26 @@ namespace VariousTypes
             Console.WriteLine("Hello LP!");
             int numero_1 = 6;
             string nome = "Nuno";
-            bool booleano = true;
+            bool booleano_1 = true;
             char character_1 = '\uFDFD';
             char character_2 = '\u2661';
             char character_3 = '\u1D4F';
             char character_4 = '\u2070';
             char character_5 = '\u0007';
+            float numero_real_1 = 4.3f;
+            double numero_real_2 = 1432.98765432f;
+            bool booleano_2 = false
             Console.WriteLine(numero_1);
             Console.WriteLine(nome);
-            Console.WriteLine(booleano);
+            Console.WriteLine(booleano_1);
             Console.WriteLine(character_1);
             Console.WriteLine(character_2);
             Console.WriteLine(character_3);
             Console.WriteLine(character_4);
             Console.WriteLine(character_5);
+            Console.WriteLine(numero_real_1);
+            Console.WriteLine(numero_real_2);
+            Console.WriteLine(booleano_2);
         }
     }
 }
