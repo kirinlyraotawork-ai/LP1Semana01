fc8512c56d176ab703b755f5df742e77299dca40
Kirin <kirinlyraota.work@gmail.com>
2026-02-18 16:01:29 +0000

    First variable alterations

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+            int numero_1 = 6;
+            string nome = "Nuno";
+            bool booleano = true;
+            Console.WriteLine(numero_1);
+            Console.WriteLine(nome);
+            Console.WriteLine(booleano);
+        }
+    }
+}
