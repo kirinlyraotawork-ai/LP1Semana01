ff377ffe99a16ec3598d026a6c7e65d3f991f2bf
Kirin <kirinlyraota.work@gmail.com>
2026-02-25 21:47:09 +0000

    last alterations on ManyStrings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,11 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+            string xx_1= $"xx={xx:f2} & also {xx:p1}";
+            string ii_1= $"you want ii in coin? here ya go ho {ii:c1}";
+            string ii_2= $"& um brinde = {ii:x1}";
             int x = 4;
             int y = 5;
             string nome_1 = "Hello guysssss \u00B1,";
@@ -25,6 +30,9 @@ namespace ManyStrings
             Console.WriteLine(abc_1);
             Console.WriteLine(s_1);
             Console.WriteLine(s_2);
+            Console.WriteLine(xx_1);
+            Console.WriteLine(ii_1);
+            Console.WriteLine(ii_2);
             
         }
     }
