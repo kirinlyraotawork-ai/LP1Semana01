2a8b3a9193efe0c94414891a566d6e321b99652a
Kirin <kirinlyraota.work@gmail.com>
2026-02-25 22:03:52 +0000

    added addition

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,7 +6,16 @@ namespace AskForInput
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+        
+            Console.WriteLine("Gimme a Num");
+            string str_1 = Console.ReadLine();
+            int a_1 = int.Parse(str_1);
+            Console.WriteLine("Gimme a real Num");
+            string str_2 = Console.ReadLine();
+            float a_2 = Convert.ToSingle(str_2);
+            Console.WriteLine($"{a_1} + {a_2} ={a_1 +a_2}");
+
+
         }
     }
 }
